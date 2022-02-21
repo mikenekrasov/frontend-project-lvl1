07c310f9799af11a0f638d79381c81fe408a624c
Mikhail Nekrasov <nekrasov_mm@pulsarnpp.ru>
2022-02-22 01:12:17 +0300

    code refactoring

---
+++ src/games/brain-even/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+import getRandomInt from '../../helpers/getRandomInt.js';
+import isEven from './isEven.js';
+import greetings from '../../helpers/greetings.js';
+
+const brainEven = () => {
+  const rounds = 3;
+  const minimum = 0;
+  const maximum = 100;
+
+  const user = greetings();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < rounds; i += 1) {
+    const randomNumber = getRandomInt(minimum, maximum);
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    const result = isEven(randomNumber);
+    if (answer !== result) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.`);
+      console.log(`Let's try again, ${user}!`);
+      return;
+    }
+    console.log('Correct');
+  }
+  console.log(`Congratulations, ${user}!`);
+};
+
+export default brainEven;
