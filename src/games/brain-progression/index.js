391fa708f609d2d2da085938f2d112b5b8b23a1e
Mikhail Nekrasov <nekrasov_mm@pulsarnpp.ru>
2022-03-09 23:30:30 +0300

    change brain-progression logic

--- src/games/brain-progression/index.js
+++ src/games/brain-progression/index.js
@@ -1,33 +1,27 @@
 import readlineSync from 'readline-sync';
+import engine from '../../engine.js';
 import getRandomInt from '../../helpers/getRandomInt.js';
-import greetings from '../../helpers/greetings.js';
 import arithmeticProgressionGenerator from './arithmeticProgressionGenerator.js';
 
 const brainProgression = () => {
-  const rounds = 3;
-  const user = greetings();
-  console.log('What number is missing in the progression?');
-  for (let i = 0; i < rounds; i += 1) {
+  const instructions = 'What number is missing in the progression?';
+
+  const makeRound = () => {
     const firstArg = getRandomInt(1, 5);
     const difference = getRandomInt(1, 3);
     const range = getRandomInt(6, 10);
 
     const progression = arithmeticProgressionGenerator(firstArg, difference, range);
     const randomIndex = getRandomInt(0, progression.length - 1);
-    const hiddenElement = progression[randomIndex];
+
     progression[randomIndex] = '..';
     const progressionWithHiddenElement = progression.join(' ');
+    const question = `Question: ${progressionWithHiddenElement}`;
+    const result = Number(readlineSync.question('Your answer: '));
+    return [question, result];
+  };
 
-    console.log(`Question: ${progressionWithHiddenElement}`);
-    const answer = Number(readlineSync.question('Your answer: '));
-    if (answer !== hiddenElement) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${hiddenElement}'.`);
-      console.log(`Let's try again, ${user}!`);
-      return;
-    }
-    console.log('Correct');
-  }
-  console.log(`Congratulations, ${user}!`);
+  engine(instructions, makeRound);
 };
 
 export default brainProgression;
