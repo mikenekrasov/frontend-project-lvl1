db2675c35e901cd75fb34467c5abd58f6d149f61
Mikhail Nekrasov <nekrasov_mm@pulsarnpp.ru>
2022-03-09 23:27:37 +0300

    change brain-gcd logic

--- src/games/brain-gcd/index.js
+++ src/games/brain-gcd/index.js
@@ -1,26 +1,19 @@
-import readlineSync from 'readline-sync';
 import getRandomInt from '../../helpers/getRandomInt.js';
-import greetings from '../../helpers/greetings.js';
 import getGreatestCommonDivisor from './getGreatestCommonDivisor.js';
+import engine from '../../engine.js';
 
 const brainCalc = () => {
-  const rounds = 3;
-  const user = greetings();
-  console.log('Find the greatest common divisor of given numbers.');
-  for (let i = 0; i < rounds; i += 1) {
+  const instructions = 'Find the greatest common divisor of given numbers.';
+
+  const makeRound = () => {
     const firstArg = getRandomInt(1, 100);
     const secondArg = getRandomInt(1, 100);
-    console.log(`Question: ${firstArg} ${secondArg}`);
-    const result = getGreatestCommonDivisor(firstArg, secondArg);
-    const answer = Number(readlineSync.question('Your answer: '));
-    if (answer !== result) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.`);
-      console.log(`Let's try again, ${user}!`);
-      return;
-    }
-    console.log('Correct');
-  }
-  console.log(`Congratulations, ${user}!`);
+    const question = `Question: ${firstArg} ${secondArg}`;
+    const result = String(getGreatestCommonDivisor(firstArg, secondArg));
+    return [question, result];
+  };
+
+  engine(instructions, makeRound);
 };
 
 export default brainCalc;
