0040f9cd437782340d9a2bd34eaf051be5a37231
Mikhail Nekrasov <nekrasov_mm@pulsarnpp.ru>
2022-03-09 23:28:46 +0300

    change brain-prime logic

--- src/games/brain-prime/index.js
+++ src/games/brain-prime/index.js
@@ -1,27 +1,16 @@
-import readlineSync from 'readline-sync';
+import engine from '../../engine.js';
 import getRandomInt from '../../helpers/getRandomInt.js';
-import greetings from '../../helpers/greetings.js';
-import yesNoAnswer from '../../helpers/yesNoAnswer.js';
 import isPrime from './isPrime.js';
 
-const brainProgression = () => {
-  const rounds = 3;
-  const user = greetings();
-  yesNoAnswer();
-  for (let i = 0; i < rounds; i += 1) {
-    const randomInt = getRandomInt(0, 100);
-    const result = isPrime(randomInt);
+const brainPrime = () => {
+  const instructions = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-    console.log(`Question: ${randomInt}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer !== result) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.`);
-      console.log(`Let's try again, ${user}!`);
-      return;
-    }
-    console.log('Correct');
-  }
-  console.log(`Congratulations, ${user}!`);
+  const makeRound = () => {
+    const randomInt = getRandomInt(0, 100);
+    const result = isPrime(randomInt) ? 'yes' : 'no';
+    return [randomInt, result];
+  };
+  engine(instructions, makeRound);
 };
 
-export default brainProgression;
+export default brainPrime;
