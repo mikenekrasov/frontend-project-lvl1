d10adb70dc04857396f481645fc1e701b5ec95ad
Mikhail Nekrasov <nekrasov_mm@pulsarnpp.ru>
2022-04-09 12:36:30 +0300

    change user input variable

--- src/engine.js
+++ src/engine.js
@@ -2,25 +2,25 @@ import readlineSync from 'readline-sync';
 
 const rounds = 3;
 
-const engine = (instructions, makeRound) => {
+const engine = (instructions, generateRound) => {
   console.log('Welcome to the Brain Games!');
-  const user = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${user}`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
   console.log(instructions);
 
   for (let i = 0; i < rounds; i += 1) {
-    const [question, answer] = makeRound();
+    const [question, answer] = generateRound();
     console.log(`Question: ${question}`);
-    const result = readlineSync.question('Your answer: ');
+    const input = readlineSync.question('Your answer: ');
 
-    if (answer !== result) {
-      console.log(`'${result}' is wrong answer ;(. Correct answer was '${answer}'. Let's try again, ${user}!`);
+    if (answer !== input) {
+      console.log(`'${input}' is wrong answer ;(. Correct answer was '${answer}'. Let's try again, ${name}!`);
       return;
     }
 
     console.log('Correct!');
   }
-  console.log(`Congratulations, ${user}!`);
+  console.log(`Congratulations, ${name}!`);
 };
 
 export default engine;
