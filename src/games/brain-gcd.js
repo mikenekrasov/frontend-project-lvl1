775dc366dcd8cc14ab318583545137e27e99ebf5
Mikhail Nekrasov <nekrasov_mm@pulsarnpp.ru>
2022-04-22 23:15:30 +0300

    add getGreatestCommonDivisor function

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,8 +1,14 @@
 import getRandomNumber from '../helpers/getRandomNumber.js';
-import getGreatestCommonDivisor from '../helpers/getGreatestCommonDivisor.js';
 import engine from '../engine.js';
 
 const instruction = 'Find the greatest common divisor of given numbers.';
+
+const getGreatestCommonDivisor = (num1, num2) => {
+  if (!num2) {
+    return num1;
+  }
+  return getGreatestCommonDivisor(num2, num1 % num2);
+};
 
 const generateRound = () => {
   const number1 = getRandomNumber(1, 100);
