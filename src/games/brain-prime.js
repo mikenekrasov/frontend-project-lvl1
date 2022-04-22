c9bcce88ed631198c5290e787c775fd7e9aab144
Mikhail Nekrasov <nekrasov_mm@pulsarnpp.ru>
2022-04-22 23:16:50 +0300

    add isPrime function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,8 +1,18 @@
 import engine from '../engine.js';
 import getRandomNumber from '../helpers/getRandomNumber.js';
-import isPrime from '../helpers/isPrime.js';
 
 const instruction = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  if (number <= 1) return false;
+
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const generateRound = () => {
   const randomNumber = getRandomNumber(0, 100);
