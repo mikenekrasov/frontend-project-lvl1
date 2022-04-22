d45b6f68336b837e0db533873d331095b25b6b7b
Mikhail Nekrasov <nekrasov_mm@pulsarnpp.ru>
2022-04-22 23:14:25 +0300

    add isEven function

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,8 +1,9 @@
 import getRandomNumber from '../helpers/getRandomNumber.js';
-import isEven from '../helpers/isEven.js';
 import engine from '../engine.js';
 
 const instruction = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
   const randomNumber = getRandomNumber(0, 100);
