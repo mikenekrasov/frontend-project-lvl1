3a4c1ff7aaa968c2ea40b1e9792bad7d3eef6681
Mikhail Nekrasov <nekrasov_mm@pulsarnpp.ru>
2022-04-22 23:50:56 +0300

    remove helpers functions

--- src/helpers/getGreatestCommonDivisor.js
+++
@@ -1,8 +0,0 @@
-const getGreatestCommonDivisor = (num1, num2) => {
-  if (!num2) {
-    return num1;
-  }
-  return getGreatestCommonDivisor(num2, num1 % num2);
-};
-
-export default getGreatestCommonDivisor;
